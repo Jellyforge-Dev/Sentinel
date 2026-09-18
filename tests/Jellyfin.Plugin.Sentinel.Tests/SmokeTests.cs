55c072b489224328bdaa078c2469ba1de6a62cd4
6AZKEKK <Philipp@Bode-Netz.de>
2026-09-18 09:25:02 +0200

    chore: add xUnit test project

---
+++ tests/Jellyfin.Plugin.Sentinel.Tests/SmokeTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Jellyfin.Plugin.Sentinel.Tests;
+
+public class SmokeTests
+{
+    [Fact]
+    public void TestProject_IsWired_Correctly()
+    {
+        Assert.True(true);
+    }
+}
